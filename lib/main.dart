89fd9f3ef009c3d9d52e87d68537fc9ea0d9e7f6
HavasAhammed <havasahammedt33@gmail.com>
2023-01-24 13:03:29 +0530

    app.dart added

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:wallpaper_hub/theme/style.dart';
-import 'package:wallpaper_hub/theme/theme_provider.dart';
-import 'package:wallpaper_hub/views/splashscreen.dart';
+import 'package:wallpaper_hub/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ThemeProvider themeChangeProvider = ThemeProvider();
-
-  @override
-  void initState() {
-    getCurrentAppTheme();
-    super.initState();
-  }
-
-  void getCurrentAppTheme() async {
-    themeChangeProvider.darkTheme =
-        await themeChangeProvider.preference.getTheme();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) {
-        return themeChangeProvider;
-      },      
-      child: Consumer<ThemeProvider>(
-          builder: (BuildContext context, value, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: Styles.themeData(themeChangeProvider.darkTheme, context),
-          title: 'Wallpaper Hub',
-          home: const SplashScreen(),
-        );
-      }),
-    );
-  }
 }
